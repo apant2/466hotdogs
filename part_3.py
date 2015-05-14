d83e8f87a4c4ee1ac9c3539b9144decb1d5160af
apant2 <anav.pant@gmail.com>
2015-05-14 15:44:09 -0500

    Moved part 3 up as the first thing to be done, as the files need to be physically created in order to do the other two analyses.

--- part_3.py
+++ part_3.py
@@ -7,7 +7,11 @@ rootdir = '/' #Rootdir is directory where the folders for the datasets are
 
 for subdir, dirs, files in os.walk(rootdir):
     for dir_name in dirs:
-        
+
+        #3)	Running	time.
+        start_time = time.monotonic()
+        #Run algorithm on one directory
+        end_time = time.monotonic()
         
         #1)	Convert	the	motif	in	motif.txt	to	a	PWM	format	and	compute	relative	entropy
         #between	“motif.txt”	and	“predictedmotif.txt”	(search	the	web	for	what	“relative
@@ -30,11 +34,6 @@ for subdir, dirs, files in os.walk(rootdir):
         overlap = set(a) & set(b)
         #count the overlap set or something
         #output result to some file
-            
-            
-        #3)	Running	time.
-        start_time = time.monotonic()
-        #Run algorithm on one directory
-    end_time = time.monotonic()
+
 
 #do some analysis or something
