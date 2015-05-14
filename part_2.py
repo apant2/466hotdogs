b57bc6481ef771baaec64059083d7c800a08d35b
apant2 <anav.pant@gmail.com>
2015-05-13 20:03:35 -0500

    Commented code for readability
    title

--- part_2.py
+++ part_2.py
@@ -3,14 +3,17 @@
 import distance
 
 def score(a, b):
-    return len(a) - distance.hamming(a,b)
+    return len(a) - distance.hamming(a,b) #hamming is the number of differences between two strings
 
+#Inputs the list of all motifs (one motif per sequence) as motif_list, and the integer of motif length (each motif has same length) as motif_len
+#Returns the position weight matrix
 def pwm(motif_list, motif_len):
-    num_A = 0
+    num_A = 0 #number of A nucleotides. Similar for next three lines
     num_C = 0
     num_G = 0
     num_T = 0
     pmotif_rows = []
+    #Fills in the position weight matrix. Traverses every character in all the motifs in motif_list
     for i in range(len(motif_list)):
         for j in range(motif_len):
             if motif_list[i][j]=='A':
@@ -21,16 +24,18 @@ def pwm(motif_list, motif_len):
                 num_G += 1
             elif motif_list[i][j]=='T':
                 num_T += 1
+        #After going through a motif, add the num values to the first row of the posittion weight matrix
         pmotif_rows.append("%d %d %d %d\n" % (num_A, num_C, num_G, num_T))
 
     return pmotif_rows
 
+#The actual algorithm.
 def find_motifs():
     with open("motiflength.txt", 'r') as f:
         mot_len = f.readlines()
     with open("sequences.fa", 'r') as f:
         seq = f.read().splitlines()
-    ML = int(mot_len[0])
+    ML = int(mot_len[0]) #cast mot_len to an integer
     m = [""] * len(seq)
     pred_sites = [""]*len(seq)
     seq_size = len(seq[1])
@@ -40,7 +45,7 @@ def find_motifs():
         for j in range(0, seq_size-ML + 1):
             test_motif_A = seq[1][i:i + ML] # the slice represents the size of the motif
             test_motif_B = seq[3][j:j + ML]
-            if score(test_motif_A, test_motif_B) > score(m[0], m[1]): # greater than previous pair
+            if score(test_motif_A, test_motif_B) > score(m[0], m[1]): # if new motif score greater than score of current motif score, replace current with new
                 m[0] = test_motif_A
                 m[1] = test_motif_B
                 pred_sites[0] = i
