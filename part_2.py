90fda6b8718e05259a6dc7c35e424fe4ecce1549
patrickchu777 <patrick.chu777@gmail.com>
2015-05-14 16:43:53 -0500

    new part 2
    rewrote to incorporate profile matrices

--- part_2.py
+++ part_2.py
@@ -1,79 +1,97 @@
 # this function scores matches as 1, mismatches as 0.
+import numpy as np
+import time
 
-import distance
 def num_lines(filename):
     return sum(1 for line in open(filename))
-def score(a, b):
-    return len(a) - distance.hamming(a,b) #hamming is the number of differences between two strings
 
-#Inputs the list of all motifs (one motif per sequence) as motif_list, and the integer of motif length (each motif has same length) as motif_len
-#Returns the position weight matrix
-def pwm(motif_list, motif_len):
-    num_A = 0 #number of A nucleotides. Similar for next three lines
-    num_C = 0
-    num_G = 0
-    num_T = 0
-    pmotif_rows = []
-    #Fills in the position weight matrix. Traverses every character in all the motifs in motif_list
-    for i in range(len(motif_list)):
-        for j in range(motif_len):
-            if motif_list[i][j]=='A':
-                num_A += 1
-            elif motif_list[i][j]=='C':
-                num_C += 1
-            elif motif_list[i][j]=='G':
-                num_G += 1
-            elif motif_list[i][j]=='T':
-                num_T += 1
-        #After going through a motif, add the num values to the first row of the posittion weight matrix
-        pmotif_rows.append("%d %d %d %d\n" % (num_A, num_C, num_G, num_T))
+def score(seq, num_seq, DNA, ML):  # number of sequences we are comparing in profile matrix
+    profile_matrix = np.zeros((4, ML))
+    occurrences = np.zeros(ML)
+    consensus_score = 0
+    for i in range(ML):
+        for j in range(num_seq):  # up to how many sequences
+            if DNA[j][int(seq[j])+i] == 'A':  # seq[j] is motif start index
+                profile_matrix[0, i] += 1
+            elif DNA[j][int(seq[j])+i] == 'C':
+                profile_matrix[1, i] += 1
+            elif DNA[j][int(seq[j])+i] == 'G':
+                profile_matrix[2, i] += 1
+            elif DNA[j][int(seq[j])+i] == 'T':
+                profile_matrix[3, i] += 1
+
+for k in range(ML):  # k represents the current position within a motif
+    for x in range(4):  # x represents which letter A,C,G,T we're iterating over
+        if profile_matrix[x, k] > occurrences[k]:
+            occurrences[k] = profile_matrix[x, k]
+        consensus_score += occurrences[k]
     
-    return pmotif_rows
+    return consensus_score
+def pwm(motif_list, ML): # seq is list of all ideal motifs. ML is motif length
+    profile_matrix = np.zeros((4, ML))
+    for i in range(ML): # go through each position in motif
+        for j in range(len(motif_list)):  # set of all motifs
+            if motif_list[j][i] == 'A':  # seq[j] is motif start index
+                profile_matrix[0, i] += 1
+            elif motif_list[j][i] == 'C':
+                profile_matrix[1, i] += 1
+            elif motif_list[j][i] == 'G':
+                profile_matrix[2, i] += 1
+            elif motif_list[j][i] == 'T':
+                profile_matrix[3, i] += 1
 
-#The actual algorithm.
+return profile_matrix.transpose()
+# Inputs the list of all motifs (one motif per sequence) as motif_list,
+# and the integer of motif length (each motif has same length) as motif_len
+# Returns the position weight matrix
+
+#  The actual algorithm.
 def find_motifs():
     with open("motiflength.txt", 'r') as f:
         mot_len = f.readlines()
     with open("sequences.fa", 'r') as f:
         seq = f.read().splitlines()
-    seq = [elem for elem in seq if len(elem) ==500] # filter out the >seq w/e lines.
-    ML = int(mot_len[0]) #cast mot_len to an integer
-    m = [""] * len(seq)
-    pred_sites = [""]*len(seq)
-    seq_size = len(seq[1])
-    m[0] = "" * ML # initialize so score() runs
-    m[1] = "" * ML
-    for i in range(0, seq_size-ML + 1):
-        for j in range(0, seq_size-ML + 1):
-            test_motif_A = seq[0][i:i + ML] # the slice represents the size of the motif
-            test_motif_B = seq[1][j:j + ML]
-            if score(test_motif_A, test_motif_B) > score(m[0], m[1]): # if new motif score greater than score of current motif score, replace current with new
-                m[0] = test_motif_A
-                m[1] = test_motif_B
-                pred_sites[0] = i
-                pred_sites[1] = j
+    seq = [elem for elem in seq if len(elem) == 500]  # filter out the >seq w/e lines.
+    ML = int(mot_len[0]) #  cast mot_len to an integer
+    best_motif = np.zeros(len(seq))
+    s = np.zeros(len(seq))
+    seq_size = len(seq[0])
+    best_score = score(best_motif, 2, seq, ML)
+    for i in range(seq_size-ML + 1):
+        s[0] = i
+        for j in range(seq_size-ML + 1):
+            s[1] = j
+            if score(s, 2, seq, ML) > score(best_motif, 2, seq, ML): # if new motif score > score of current motif score, replace current with new
+                best_motif[0] = s[0]
+                best_motif[1] = s[1]
 
-for i in range(2, len(seq)): # go through set of all sequences
-    m[i] = "V" * ML
-        for j in range(0, seq_size-ML+1): # go through potential motifs in each sequence
-            tmot = seq[i][j:j + ML] # current best motif match
-            if score(m[0], tmot) > score(m[0], m[i]):
-                # maybe use this? and score(m[1], tmot) >= score(m[1], m[i])
-                m[i] = tmot
-                pred_sites[i] = j
+s[0] = best_motif[0]
+    s[1] = best_motif[1]
+    
+    for i in range(2, len(seq)): # go through set of all sequences
+        for j in range(seq_size-ML+1): # go through potential motifs in each sequence
+            s[i] = j
+            s = score(s, i+1, seq, ML)
+            if s > best_score:
+                best_score = s
+                s[i] = j
+                best_motif[i] = j
 
-print(m)
-    print(pred_sites)
-    
-    psites = open("predictedsites.txt", 'w')
-    for i in range(len(pred_sites)):
-        psites.write(str(pred_sites[i])+'\n')
-    
-    pmotif = open("predictedmotif.txt", 'w')
+psites = open("predictedsites.txt", 'w')
+    for i in range(len(best_motif)):
+        psites.write(str(best_motif[i])+'\n')
+
+pmotif = open("predictedmotif.txt", 'w')
     pmotif.write(">PMOTIF " + str(ML) + "\n")
-    pwm_rows = pwm(m, ML)
+    pwm_rows = pwm(seq, ML)
+    pwm_rows = pwm_rows/len(seq)
     for i in range(len(pwm_rows)):
-        pmotif.write(pwm_rows[i])
-    pmotif.write("<")
+        pmotif.write(str(pwm_rows[i, 0]) + " " + str(pwm_rows[i, 1]) + " " + str(pwm_rows[i, 2]) + " " + str(pwm_rows[i, 3]) + "\n")
+pmotif.write("<")
 
+#-------Function Call--------
+start = time.time()
 find_motifs()
+end = time.time()
+
+print(end - start)
